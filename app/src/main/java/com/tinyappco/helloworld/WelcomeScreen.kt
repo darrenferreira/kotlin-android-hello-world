5942153fb01a65d835558e35410abccd451be283
Ntimana <Fortune.Ntimana@standardbank.co.za>
2023-12-10 12:52:08 +0200

    Navigation of the home screen

--- app/src/main/java/com/tinyappco/helloworld/WelcomeScreen.kt
+++ app/src/main/java/com/tinyappco/helloworld/WelcomeScreen.kt
@@ -1,5 +1,6 @@
 package com.tinyappco.helloworld
 
+import android.app.FragmentManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +10,7 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class WelcomeScreen : Fragment() {
 
@@ -26,6 +28,24 @@ class WelcomeScreen : Fragment() {
         val info = data?.getString(NAME)
         myTextView.text = info
 
+        val firstFragment=MainScreen()
+        val secondFragment=WelcomeScreen()
+        val thirdFragment=HomeFragment()
+        val bottomNavigationView=view.findViewById<BottomNavigationView>(R.id.bottomNavigationView1)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.home->setCurrentFragment(thirdFragment)
+
+
+
+            }
+            true
+        }
+
         return view
     }
+    private fun setCurrentFragment(fragment1:Fragment)=
+        fragmentManager?.beginTransaction()?.apply { replace(R.id.fragmentContainerView,fragment1).commit()}
+
 }
