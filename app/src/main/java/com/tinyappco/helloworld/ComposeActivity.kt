d92c5f067856b4e93a2242367c7ef857eff8098a
Masuta <Tadiwa.Masuta@standardbank.co.za>
2024-01-10 13:44:54 +0200

    Changes to the compose activity

--- app/src/main/java/com/tinyappco/helloworld/ComposeActivity.kt
+++ app/src/main/java/com/tinyappco/helloworld/ComposeActivity.kt
@@ -1,11 +1,17 @@
 package com.tinyappco.helloworld
 
+import android.graphics.fonts.FontFamily
+import android.graphics.fonts.FontStyle
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CardDefaults
@@ -17,7 +23,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -41,13 +49,32 @@ fun Transactions() {
         ),
         border = BorderStroke(1.dp, Color.Black),
         modifier = Modifier
-            .size(width = 240.dp, height = 100.dp)
+            .size(width = 240.dp, height = 200.dp)
+        ,
+
     ) {
         Text(
-            text = "Outlined",
-            modifier = Modifier
-                .padding(16.dp),
-            textAlign = TextAlign.Center,
+                text = "Transactions",
+                color = Color.Black,
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                textDecoration = TextDecoration.None
+            )
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Text(
+            text = "R2000 - New Balance 550",
+            color = Color.Black,
+            fontSize = 15.sp,
+            textDecoration = TextDecoration.None
+        )
+        Text(
+            text = "R3000 - Groceries",
+            color = Color.Black,
+            fontSize = 15.sp,
+            textDecoration = TextDecoration.None
         )
     }
 }
