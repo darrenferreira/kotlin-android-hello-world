bc2552bbfa1389b65473fdf09b1e60dae4402257
Masuta <Tadiwa.Masuta@standardbank.co.za>
2024-01-17 10:06:25 +0200

    Final changes to compose screen

--- app/src/main/java/com/tinyappco/helloworld/ComposeActivity.kt
+++ app/src/main/java/com/tinyappco/helloworld/ComposeActivity.kt
@@ -1,35 +1,26 @@
 package com.tinyappco.helloworld
 
-import android.graphics.fonts.FontFamily
-import android.graphics.fonts.FontStyle
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.tinyappco.helloworld.ui.theme.HelloWorldTheme
+
 
 class ComposeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,6 +34,7 @@ class ComposeActivity : ComponentActivity() {
 @Preview
 @Composable
 fun Transactions() {
+
     OutlinedCard(
         colors = CardDefaults.cardColors(
             containerColor = MaterialTheme.colorScheme.surface,
@@ -50,16 +42,15 @@ fun Transactions() {
         border = BorderStroke(1.dp, Color.Black),
         modifier = Modifier
             .size(width = 240.dp, height = 200.dp)
-        ,
-
     ) {
+
         Text(
-                text = "Transactions",
-                color = Color.Black,
-                fontSize = 30.sp,
-                fontWeight = FontWeight.Bold,
-                textAlign = TextAlign.Center,
-                textDecoration = TextDecoration.None
+            text = "Transactions",
+            color = Color.Black,
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            textDecoration = TextDecoration.None
             )
 
         Spacer(modifier = Modifier.height(10.dp))
